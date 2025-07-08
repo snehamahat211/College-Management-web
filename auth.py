a297d413a956423181326cb93ecfe0e626ce867d
snehamahat211 <snehamahat211@gmail.com>
2025-07-08 18:57:36 +0545

    created accesss token

--- auth.py
+++ auth.py
@@ -8,3 +8,12 @@ SECRET_KEY="your_secret_key"
 ALGORITHM="HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES=30
 oauth2_scheme=OAuth2PasswordBearer(tokenUrl="login")
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+
