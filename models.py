a29cd8792e29041cf5fa58aa329afa5b4fda8687
snehamahat211 <snehamahat211@gmail.com>
2025-07-09 08:34:43 +0545

    modification of models.py

--- models.py
+++ models.py
@@ -1,11 +1,12 @@
-from pydantic import BaseModel
+from sqlmodel import SQLModel, Field
+from typing import Optional
 
-class User(BaseModel):
-    username:str
-    password:str
+class User(SQLModel):
+    username: str
+    password: str
 
-class Student(BaseModel):
-    id:int
-    name:str
-    department:str
-    
+class Student(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    age: int
+    grade: str
